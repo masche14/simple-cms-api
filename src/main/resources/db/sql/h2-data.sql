fddfe53d1a5ee2cb54681971a11bd7a2a8036f5d
masche14 <zskfnxh134@gmail.com>
2026-03-12 14:38:19 +0900

    과제 기본 환경 설정 및 H2 DB 초기화 스크립트 작성

--- src/main/resources/db/sql/h2-data.sql
+++ src/main/resources/db/sql/h2-data.sql
@@ -1,3 +1,8 @@
--- TODO data
--- example
-insert into members (name, last_modified_date) values('bob', now());
+INSERT INTO users (user_id, password, role, register_date, register_by)
+VALUES (
+           'admin',
+           '$2a$10$CQaEpR9uETh5FY7tlc0TKODWCeVWATmw4ey..cvjhO7Mb1YWOqxgK',
+           'ADMIN',
+           NOW(),
+           'system'
+       );
--- src/main/resources/db/sql/h2-schema.sql
+++ src/main/resources/db/sql/h2-schema.sql
@@ -1,9 +1,25 @@
--- TODO schema
--- example
-create table members
+DROP TABLE IF EXISTS contents CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+
+-- 사용자 테이블
+CREATE TABLE users
 (
-    id                 bigint primary key      not null auto_increment,
-    name               varchar(50)             not null,
-    created_date       timestamp default now() not null,
-    last_modified_date timestamp
+    user_id             VARCHAR(50) PRIMARY KEY  NOT NULL UNIQUE,
+    password           VARCHAR(255)             NOT NULL,
+    role               VARCHAR(20)              NOT NULL,
+    register_date      TIMESTAMP DEFAULT NOW()  NOT NULL,
+    register_by        VARCHAR(50)
+);
+
+-- 콘텐츠 테이블
+CREATE TABLE contents
+(
+    id                 BIGINT PRIMARY KEY      NOT NULL AUTO_INCREMENT,
+    title              VARCHAR(100)            NOT NULL,
+    description        TEXT,
+    view_count         BIGINT                  NOT NULL,
+    created_date       TIMESTAMP,
+    created_by         VARCHAR(50)             NOT NULL,
+    last_modified_date TIMESTAMP,
+    last_modified_by   VARCHAR(50)
 );
