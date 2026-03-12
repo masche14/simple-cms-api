b9bac98c4ba4cbf5adc9dd0b105b15248b1dd851
masche14 <zskfnxh134@gmail.com>
2026-03-12 14:36:47 +0900

    초기 DB 데이터 세팅을 위한 BCrypt 해시 생성 유틸리티 작성

---
+++ src/main/java/com/malgn/util/BcryptHash.java
@@ -0,0 +1,10 @@
+package com.malgn.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BcryptHash {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("1234"));
+    }
+}
