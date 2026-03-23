d8aca4ca6696ee12692bc84e75fbddbcaa6f094f
masche14 <zskfnxh134@gmail.com>
2026-03-24 03:09:58 +0900

    UserInfoDTO 수정

--- src/main/java/com/malgn/dto/user/UserInfoDTO.java
+++ src/main/java/com/malgn/dto/user/UserInfoDTO.java
@@ -2,9 +2,11 @@ package com.malgn.dto.user;
 
 import com.malgn.auth.Role;
 import com.malgn.entity.User;
+import lombok.Builder;
 
 import java.time.LocalDateTime;
 
+@Builder
 public record UserInfoDTO(
         String userId,
         String password,
@@ -13,12 +15,12 @@ public record UserInfoDTO(
         String registerBy
 ) {
     public static UserInfoDTO from (User user) {
-        return new UserInfoDTO(
-                user.getUserId(),
-                user.getPassword(),
-                user.getRole(),
-                user.getRegisterDate(),
-                user.getRegisterBy()
-        );
+        return UserInfoDTO.builder()
+                .userId(user.getUserId())
+                .password(user.getPassword())
+                .role(user.getRole())
+                .registerDate(user.getRegisterDate())
+                .registerBy(user.getRegisterBy())
+                .build();
     }
 }
